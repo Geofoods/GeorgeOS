cf888e896252ccf28f298c452f547e2f1c8cca8d
Geofoods <curious.george71511@gmail.com>
2026-08-10 11:56:14 -0400

    instagram icon

--- app.js
+++ app.js
@@ -1766,7 +1766,7 @@ function buildInstagramApp(windowElement) {
   const shell = document.createElement('div');
   shell.className = 'instagram-shell';
   shell.innerHTML = `
-    <img class="instagram-banner" src="https://th.bing.com/th/id/R.26d9974a1feec9905a4e0d5e5ddf8db6?rik=Og1ujXM2C1AJHQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fa%2fa5%2fInstagram_icon.png&ehk=1%2fZWXYn2nN%2fR80TOtcKH5SsdLkkUvMLrB%2fHUXRDHk9I%3d&risl=&pid=ImgRaw&r=0" alt="" aria-hidden="true" />
+    <img class="instagram-banner" src="icons/OIP.jpg" alt="" aria-hidden="true" />
     <h2>Instagram</h2>
     <p class="instagram-description">
       @georgesunreal
